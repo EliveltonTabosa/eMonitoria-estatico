75f75e5315db3ac858a69a18a26cbee128472d9b
Elivelton Tabosa <elivelton_br@hotmail.com>
2022-01-27 00:07:17 -0300

    Armazenamento1

--- script_eMonitoria.js
+++ script_eMonitoria.js
@@ -22,10 +22,10 @@ let nomePonto2 =[]
         const itens = document.getElementById('itens')
         for (let index = 0; index < pontos.length; index++) {
             itens.innerHTML += `<div>
-            <input type="radio" name="t${index}" id="Não" checked >
-            <label for="não">sim</label>
-            <input type="radio" name="t${index}" id="Sim">
-            <label for="sim">não</label>
+            <input type="radio" name="t${index}" id="sim${index}" checked onclick="lançar()">
+            <label for="sim">sim</label>
+            <input type="radio" name="t${index}" id="não${index}" onclick="lançar()">
+            <label for="não">não</label>
             <span id="tex${index}">     -     </span> 
             </div>`
         }
@@ -41,10 +41,11 @@ let nomePonto2 =[]
         var notaFinal = document.getElementById("resultados")//span da nota no HTML
         var foto1 = document.getElementById("foto1")
         var radyn = []
+       
 
         for (let e = 0; e < pontos.length; e++) {
             radyn.push(document.getElementsByName(`t${e}`))
-            console.log(radyn[e])
+            // console.log(radyn[e][0])
         }
         
 
@@ -74,6 +75,44 @@ let nomePonto2 =[]
     img.removeAttribute('src','midia/Joinha.png')
    }
    foto1.appendChild(img) //joga a imagem na div da var foto1
-   console.log(pontos)  
+   //console.log(pontos)  
 }
  
+//Armazenar monitorias
+    var arrayMonitorias = []
+
+function armazenar(){   
+
+    var descriçãoMonitoriaElemento = document.getElementById('descrição')
+    var descriçãoMonitoria=descriçãoMonitoriaElemento.value
+    var nomeTeleatendenteElemento = document.getElementById('nomeTeleatendente')
+    var nomeTeleatendente =  nomeTeleatendenteElemento.value
+    var equipeTeleatendenteElemento = document.getElementById("equipeTeleatendente")
+    var equipeTeleatendente = equipeTeleatendenteElemento.value
+        
+    let monitoria = {
+        operador: nomeTeleatendente,
+        equipe: equipeTeleatendente,
+        arrayMonitoria: pontos,
+        nota: nota,
+        descrição: descriçãoMonitoria,
+        data:   new Date()
+    }
+
+    arrayMonitorias.push(monitoria)
+    alert('Monitoria Incluida!')
+    console.log(arrayMonitorias)
+
+    descriçãoMonitoriaElemento.value=""
+    nomeTeleatendenteElemento.value=""
+    equipeTeleatendenteElemento.value=""
+
+    marcarTudoSim()
+    
+}
+
+function marcarTudoSim()    {
+    for (let o = 0; o < pontos.length; o++) {
+        document.getElementById(`sim${o}`).click()
+    }
+}
